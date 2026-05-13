430d1a7439d308bd6e984fd763e10e969d924fc7
JFE <117161265+JustForEducate@users.noreply.github.com>
2026-05-13 22:41:13 +0300

    Delete restart-servers.ps1

--- restart-servers.ps1
+++
@@ -1,8 +0,0 @@
-# OpenRoadMap - Restart Script
-$host.UI.RawUI.WindowTitle = "OpenRoadMap - Restarting Servers"
-
-Write-Host ""
-Write-Host "Restarting OpenRoadMap servers..."
-Stop-Process -Name "node" -Force -ErrorAction SilentlyContinue
-Start-Sleep -Seconds 2
-Write-Host "Done. Run START.bat to start servers."
